8499b615c8060592851dc51a72cf999fda85f730
ehrennnn <lianehrenbacani@gmail.com>
2025-11-27 20:54:21 +0800

    added templates

--- MSYS30/class_scheduler/class_scheduler_app/views.py
+++ MSYS30/class_scheduler/class_scheduler_app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def hello_world(request): 
-    return render (request, 'class_scheduler_app/hello_world.html')
+    return render (request, 'class_scheduler_app/Home_page.html')
